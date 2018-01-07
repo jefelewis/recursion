70f5e9bee1c49a1283002bab87ef62e9ae637da2
jefelewis <jeffylew77@gmail.com>
2018-01-07 12:26:51 -0800

    Added tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,19 +6,42 @@
 var stringifyJSON = function(obj){
   // HRP: MODIFED
   // Check to see if input is a string
-  if(typeof obj = "string"){
-
+  if(typeof obj === "string" || typeof obj === "number" || typeof obj === "boolean"){
+    return "'" + obj + "'"
   }
 
   // Check to see if input is an array
-  else if(typeof obj = "array"){
+  else if(Array.isArray(obj)){
 
   }
 
 
   // Check to see if input is a object
-  else if(typeof obj = "object"){
+  else if(typeof obj === "object"){
 
   }
+  else{
 
-};
+  }
+}
+
+
+// String Test
+console.log(stringifyJSON("String"));
+
+// Number Test
+console.log(stringifyJSON(10));
+
+// Boolean Test
+console.log(stringifyJSON(true));
+
+// Undefined Test
+console.log(stringifyJSON());
+
+// Objects Test
+console.log(stringifyJSON({Name: "Jeff", Age: 25, Tall: true, Hobbies: null}));
+
+// Arrays Test
+console.log(stringifyJSON(["Jeff", 25, true, null]));
+
+// Function Test?
