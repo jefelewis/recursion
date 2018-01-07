65f622d210034e6e61ffd176131157a546850f0a
jefelewis <jeffylew77@gmail.com>
2018-01-06 18:30:32 -0800

    Skeleton for Stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,22 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj){
+  // HRP: MODIFED
+  // Check to see if input is a string
+  if(typeof obj = "string"){
+
+  }
+
+  // Check to see if input is an array
+  else if(typeof obj = "array"){
+
+  }
+
+
+  // Check to see if input is a object
+  else if(typeof obj = "object"){
+
+  }
+
 };
